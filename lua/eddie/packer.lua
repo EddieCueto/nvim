89b0872d5a77d299b7cff3be0092cf38315189c5
Eduardo Cueto-Mendoza <cueto303@gmail.com>
2023-07-21 14:13:54 +0100

    Using autopairs, added terminal

--- lua/eddie/packer.lua
+++ lua/eddie/packer.lua
@@ -64,15 +64,15 @@ return require('packer').startup(function(use)
     -- :FixWhitespace
     use "bronson/vim-trailing-whitespace"
 
+    -- Terminal
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+    end}
+
     -- Parenthesis complete
-    use({
-        "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    })
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 
 end)
