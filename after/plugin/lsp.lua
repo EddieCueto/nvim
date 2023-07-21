137b24d4c4c96ebcbb0bd00e29af376d443c636d
Eduardo Cueto-Mendoza <cueto303@gmail.com>
2023-07-21 21:19:38 +0100

    New FreeBSD branch

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,77 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- Language servers
+lsp.ensure_installed({
+  -- Replace these with whatever servers you want to install
+  'bashls',
+  'clangd',
+  'julials',
+  'lua_ls',
+  'ltex',
+  'pylsp',
+  'texlab',
+  'zls'
+})
+
+--[[
+    START LANGUAGE SERVERS CONFIG HERE!!!
+]]--
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+
+require('lspconfig').texlab.setup({})
+
+require('lspconfig').ltex.setup({
+  settings = {
+		ltex = {
+			language = "en-GB",
+		},
+	},
+})
+
+require('lspconfig').pylsp.setup({})
+
+--require('lspconfig').gopls.setup({
+--    on_attach = on_attach,
+--})
+
+require('lspconfig').julials.setup({
+    --on_attach = on_attach,
+    symbol_cache_download = true,
+    --symbol_server = "https://symbol-server",
+    on_new_config = function(new_config, _)
+        local julia = vim.fn.expand("/usr/share/julia/bin/julia")
+        if require'lspconfig'.util.path.is_file(julia) then
+	    vim.notify("Hello!")
+            new_config.cmd[1] = julia
+        end
+    end
+})
+
+require('lspconfig').zls.setup({})
+
+require('lspconfig').bashls.setup({})
+
+-- source: https://rust-analyzer.github.io/manual.html#nvim-lsp
+--require('lspconfig').rust_analyzer.setup({
+--    on_attach = on_attach,
+--    settings = {
+--        ["rust-analyzer"] = {
+--            inlayHints = {
+--                closingBraceHints = true, -- Whether to show inlay hints after a closing } to indicate what item it belongs to.
+--            }
+--        }
+--    }
+--})
+
+--[[
+    END LANGUAGE SERVERS CONFIG HERE!!!
+]]--
+
+lsp.setup()
---
+++ lua/eddie/keymaps.lua
@@ -0,0 +1,78 @@
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+-- delta: 2 lines
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- open File Explorer
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Navigate buffers
+vim.keymap.set("n", "<BS>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-TAB>", ":bprevious<CR>", opts)
+
+-- Telescope
+local builtin = require("telescope.builtin")
+--vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+--vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+--vim.keymap.set("n", "<leader>ps", function ()
+--    builtin.grep_string( { search = vim.fn.input("Grep > ") } );
+--end)
+
+-- Harpoon
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function () ui.nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function () ui.nav_file(2) end)
+vim.keymap.set("n", "<C-n>", function () ui.nav_file(3) end)
+vim.keymap.set("n", "<C-s>", function () ui.nav_file(4) end)
+
+-- Undotree
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+-- Fugitive
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- Put line bellow in-front of line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Don't loose buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+
+-----------------
+-- Visual mode --
+-----------------
+
+-- Hint: start visual mode with the same area as the previous area and the same mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Move text on visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+
+
+
