13e1cd6804e9d1afb22a5b353e7a762a5a169881
Eduardo Cueto-Mendoza <cueto303@gmail.com>
2023-09-07 12:53:38 +0100

    Floating terminal is better

--- lua/eddie/lazy.lua
+++ lua/eddie/lazy.lua
@@ -258,7 +258,7 @@ local plugins = {
         'voldikss/vim-floaterm',
         config = function()
             vim.keymap.set("n", "<leader>ft",
-                "<cmd>:FloatermNew --height=0.3 --width=0.8 --wintype=split --name=floaterm1 --position=center --autoclose=2<CR>", { desc = "Open FloatTerm" })
+                "<cmd>:FloatermNew --height=0.3 --width=0.8 --wintype=float --name=floaterm1 --position=center --autoclose=2<CR>", { desc = "Open FloatTerm" })
             vim.keymap.set("n", "<leader>flt",
                 "<cmd>:FloatermToggle<CR>", { desc = "Toggle FloatTerm" })
             vim.keymap.set("t", "<leader>flt",
